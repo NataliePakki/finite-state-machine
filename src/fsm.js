52279ffab9bc2c46f1d6816a069ab4a43cade4d3
MINSK\Natalia_Kravchenia <natalie.pakki@gmail.com>
2017-09-14 17:31:40 +0300

    Revert "Revert "one part of solution""
    This reverts commit 796dea9e5e27fcbfef28178264b2a7b11dadbb7a.

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,79 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(config === undefined)
+            throw new Error("config must not be undefined.");
+        this.states = config.states;
+        this.transition = [ config.initial ];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.transition[this.transition.length - 1];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this.states[state] === undefined) {
+            throw new Error("state doesn't exist"); 
+        }
+        
+        this.transition[this.transition.length - 1] = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+
+    // states: {
+    //     normal: {
+    //         transitions: {
+    //             study: 'busy',
+    //         }
+    //     },
+    //     busy: {
+    //         transitions: {
+    //             get_tired: 'sleeping',
+    //             get_hungry: 'hungry',
+    //         }
+    //     },
+    //     hungry: {
+    //         transitions: {
+    //             eat: 'normal'
+    //         },
+    //     },
+    //     sleeping: {
+    //         transitions: {
+    //             get_hungry: 'hungry',
+    //             get_up: 'normal',
+    //         },
+    //     },
+    // }
+    trigger(event) {
+        var currState = getState();
+        Object.keys(this.states[currState].transitions).forEach(function(i) {
+            if(i[event] != undefined) {
+                this.transition.push(i);
+                return;
+            }
+        }, this);
+        return false;
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.transition = [ this.transition[0] ];
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,14 +83,31 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if(event === undefined) {
+            return Object.keys(this.states);
+        }
+        var states = [];
+
+        Object.keys(this.states).forEach(function(i) {
+            var state = this.states[i];
+            if(state.transitions[event] != undefined)
+                states.push(i);
+        }, this);
+        return states;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this.transition.length === 1) {
+            return false;
+        }
+        this.transition.pop();
+    }
 
     /**
      * Goes redo to state.
@@ -53,7 +119,9 @@ class FSM {
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.transition = [];
+    }
 }
 
 module.exports = FSM;
